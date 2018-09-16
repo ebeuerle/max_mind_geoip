a6029c17702ed849e0265adc3353bd8518e23445
ebeuerle <eddie.beuerlein@gmail.com>
2018-09-16 11:24:53 -0400

    Fixed indent issue.

--- runner.py
+++ runner.py
@@ -10,13 +10,14 @@ class GeoIP_apache():
 
     def run(self):
         self.apache_line = self.file.read_file()
+        clfParts = CLFParser.logParts(apache_line,'%h %r')
+        print clfParts
 
-
-def main():
-    #sys.stdout = lib.Logger(logging.info)
-    #sys.stderr = lib.Logger(logging.warning)
-    top_geoip_apache = GeoIP_apache()
-    top_geoip_apache.run()
+    def main():
+        #sys.stdout = lib.Logger(logging.info)
+        #sys.stderr = lib.Logger(logging.warning)
+        top_geoip_apache = GeoIP_apache()
+        top_geoip_apache.run()
 
 if __name__ == "__main__":
 main()
