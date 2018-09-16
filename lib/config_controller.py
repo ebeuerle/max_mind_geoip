c3ea146d9deadb8bbb51190cf15ef2dd5e5eca7a
ebeuerle <eddie.beuerlein@gmail.com>
2018-09-16 15:14:25 -0400

    Fixed typo.

--- lib/config_controller.py
+++ lib/config_controller.py
@@ -1,5 +1,5 @@
 import os
 import yaml
 
-FILE_CONFIG = os.path.join(os.path.dirname(__file__), '../config/config.yml')
+FILE_CONFIG = os.path.join(os.path.dirname(__file__), '../configs/config.yml')
 CONFIG = yaml.load(file(FILE_CONFIG, 'r'))
