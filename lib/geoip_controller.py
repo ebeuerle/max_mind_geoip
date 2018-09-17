6941abdd20b5ece91e8590ecff3a0e3cc8a1a5cd
ebeuerle <eddie.beuerlein@gmail.com>
2018-09-17 10:44:14 -0400

    Fixed variable definition for dbreader.

--- lib/geoip_controller.py
+++ lib/geoip_controller.py
@@ -6,11 +6,11 @@ class GeoIPController(object):
     def __init__(self):
         dbdirpath = lib.CONFIG["database_directory"]
         path_to_db = os.path.join(os.path.dirname(__file__), '../', dbdirpath, 'GeoLite2-City.mmdb')
-        dbreader = geoip2.database.Reader(path_to_db)
+        self.dbreader = geoip2.database.Reader(path_to_db)
 
     def query_geoip_db(self, ip):
-        response = dbreader.city(ip)
+        response = self.dbreader.city(ip)
         return response
 
     def close_geoip_db(self):
-        dbreader.close()
+        self.dbreader.close()
