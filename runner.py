bc0205695ef9c8cf69d3ceb1956f9644690c880a
ebeuerle <eddie.beuerlein@gmail.com>
2018-09-18 17:46:00 -0400

    Refactored to use different type of dictionary and added report functions.

--- runner.py
+++ runner.py
@@ -1,31 +1,62 @@
 import lib
 from clfparser import CLFParser
-
-
+from collections import OrderedDict
+from collections import defaultdict
 class GeoIP_apache():
     def __init__(self):
         self.file = lib.FileController()
         self.geoip_data = lib.GeoIPController()
         self.sanity = lib.Sanity()
+        self.other_country = defaultdict(list)
+        self.usa = defaultdict(list)
         #sys.tracebacklimit = 0
 
+    def build_dictionary(self, country_name, state, request_m):
+        if country_name == "United States":
+            return self.usa[state].append(request_m)
+        return self.other_country[country_name].append(request_m)
+
+    def calculate_top_10(self, r_type):
+        if r_type == "country":
+            modified_dict = OrderedDict(sorted(self.other_country.items(), key=lambda item: len(item[1]), reverse=True)[:10])
+        else:
+            modified_dict = OrderedDict(sorted(self.usa.items(), key=lambda item: len(item[1]), reverse=True)[:10])
+
+        for k, v in modified_dict.iteritems():
+            modified_dict[k] = max(v,key=v.count)
+        return modified_dict
+
+    def report(self, r_type):
+        if r_type == "country":
+            data = self.calculate_top_10("country")
+        else:
+            data = self.calculate_top_10("usa")
+
+        for k, v in data.iteritems():
+            print "location: %s \t\t %s" % (k, v)
+        return None
+
     def run(self):
-        geo = dict()
+        geo = defaultdict(list)
         self.apache_file = self.file.read_file()
         for line in self.apache_file:
             clfParts = CLFParser.logParts(line,'%h %r')
             if self.sanity.match_regex(clfParts[1]):
                 pass
             else:
-                self.resp = self.geoip_data.query_geoip_db(clfParts[0])
-                geo[clfParts[0]] = {'country' : self.resp.country.name, 'state' : self.resp.subdivisions.most_specific.name, 'request' : clfParts[1]}
-        print geo
+                try:
+                    resp = self.geoip_data.query_geoip_db(clfParts[0])
+                except:
+                    pass
+                self.build_dictionary(resp.country.name, resp.subdivisions.most_specific.name, clfParts[1])
+        print self.report("country")
+        print self.report("usa")
 
 def main():
     #sys.stdout = lib.Logger(logging.info)
     #sys.stderr = lib.Logger(logging.warning)
     top_geoip_apache = GeoIP_apache()
     top_geoip_apache.run()
-
+    
 if __name__ == "__main__":
     main()
